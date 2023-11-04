26d44d8c1a157f02ab37506a575c7a58f29cb500
Nguyễn Quang Huy <nguyenquanghuylt2002@gmail.com>
2023-11-04 13:02:10 +0700

    Add function send email

--- libraries/Sendmail.php
+++ libraries/Sendmail.php
@@ -1,3 +1,34 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+function SendMail($to, $subject, $message) {
+    $ini_file = parse_ini_file(__DIR__."/../php.config.ini");
+    $senderName = $ini_file["smtp_sendername"];
+    $name = $ini_file["smtp_username"];
+    $pass = $ini_file["smtp_userpass"];
+    $host = $ini_file["smtp_host"];
+    $port = $ini_file["smtp_port"];
+    $mail = new PHPMailer(true);
+    try {
+        $mail->CharSet    = 'UTF-8';
+        $mail->SMTPDebug  = 0;
+        $mail->isSMTP();
+        $mail->SMTPAuth   = true;
+        $mail->SMTPSecure = 'tls';
+        $mail->Username   = $name;
+        $mail->Password   = $pass;
+        $mail->Host       = $host;
+        $mail->Port       = $port;
+        $mail->setFrom($name, $senderName);
+        $mail->addAddress($to);
+        $mail->isHTML(true);
+        $mail->Subject    = $subject;
+        $mail->Body       = $message;
+        return $mail->send();
+    } catch(Exception $e) {
+        throw new Exception("Có vấn đề trong quá trình gửi email! - $e");
+    }
+    return false;
+}
 ?>
