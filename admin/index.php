25155671fabd2d66d6173782a3d2da99b8f1bac3
Nguyễn Quang Huy <nguyenquanghuylt2002@gmail.com>
2023-11-15 22:13:33 +0700

    Update admin site

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,14 @@
-<?=require_once "layouts/header.php"?>
+<?php
+session_start();
+if(!isset($_SESSION["user"]) || $_SESSION["user"]["permission"] < 1) {
+    header("location:/hostay/admin/login.php?err=permis");
+}
 
-<?=require_once "layouts/footer.php"?>
+require_once "layouts/header.php";
+?>
+<!--Start main page-->
+<h1>Admin page</h1>
+<!--End main page-->
+<?php
+require_once "layouts/footer.php";
+?>
