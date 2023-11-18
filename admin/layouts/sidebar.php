006ac5de25837199c9e5e29d4b4cffcd82ff7b9c
Nguyễn Quang Huy <106138005+huynq-fouj@users.noreply.github.com>
2023-11-18 23:09:06 +0700

    Update sidebar.php

--- admin/layouts/sidebar.php
+++ admin/layouts/sidebar.php
@@ -11,6 +11,12 @@ return "show";
 }
 return "";
 }
+function getCollapsed(string $pos) {
+if(isset($_SESSION["pos"] && $_SESSION["pos"] == $pos){
+return "";
+}
+return "collapsed";
+}
 ?>
 <!-- ======= Sidebar ======= -->
 <aside id="sidebar" class="sidebar">
@@ -18,14 +24,14 @@ return "";
 <ul class="sidebar-nav" id="sidebar-nav">
 
   <li class="nav-item">
-    <a class="nav-link " href="index.html">
+    <a class="nav-link <?=getCollapsed("dashboard")?>" href="index.html">
       <i class="bi bi-grid"></i>
       <span>Dashboard</span>
     </a>
   </li><!-- End Dashboard Nav -->
 
   <li class="nav-item">
-    <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+    <a class="nav-link <?=getCollapsed("user")?>" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
         <i class="bi bi-people"></i><span>Người sử dụng</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
     <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
