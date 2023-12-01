83c037134adedf4412f52a7af2eda20890980325
Nguyễn Quang Huy <nguyenquanghuylt2002@gmail.com>
2023-12-01 15:12:46 +0700

    Delete data

--- app/lib/connection.php
+++ app/lib/connection.php
@@ -5,6 +5,7 @@ function getConnection() : mysqli {
                     $db_ini["db_user"],
                     $db_ini["db_pass"],
                     $db_ini["db_name"]);
+    $con->set_charset("utf8");
     if($con->connect_error){
         die("Không thể kết nối cơ sở dữ liệu: " . $con->connect_error);
     }
