a359196bb769b8949710fffe520724de58bcbdcb
Nguyen Van Dung <nguyenvandung.official@gmail.com>
2024-06-26 11:12:08 +0700

    add "/"

--- logout.php
+++ logout.php
@@ -3,8 +3,8 @@ session_start();
 unset($_SESSION["user"]);
 if(isset($_GET["redirect"])) {
     if($_GET["redirect"] == "admin") {
-        header("location:/hostay/admin");
+        header("location:/hostay/admin/");
     }
 } else {
-    header("location:/hostay/views");
+    header("location:/hostay/views/");
 }
